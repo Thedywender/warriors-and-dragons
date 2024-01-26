aedb2bd5460e7dbd3e2ce17585430cbb1da6aa4a
Thedywender <thedywender@hotmail.com>
2024-01-26 16:30:12 -0400

    req09 finished

--- src/Monster.ts
+++ src/Monster.ts
@@ -1,4 +1,4 @@
-import Fighter, { SimpleFighter } from './Fighter';
+import { SimpleFighter } from './Fighter';
 
 export default class Monster implements SimpleFighter {
   private _lifePoints: number;
@@ -17,16 +17,41 @@ export default class Monster implements SimpleFighter {
     return this._strength;
   }
 
-  receiveDamage(attackPoints: number): number {
-    const damage = this._lifePoints - attackPoints;
+  public receiveDamage(attackPoints: number): number {
+    // if (attackPoints <= 0) {
+    //   return this._lifePoints;
+    // }
+    const newLife = this._lifePoints - attackPoints;
 
-    if (this._lifePoints - damage <= 0) {
+    if (newLife > 0) {
+      this._lifePoints = newLife;
+    }
+    if (newLife <= 0) {
       this._lifePoints = -1;
     }
+
     return this._lifePoints;
   }
 
-  attack(enemy: Fighter | SimpleFighter): void {
+  public attack(enemy: SimpleFighter): void {
     enemy.receiveDamage(this._strength);   
   }
 }
+
+// for (let j = 0; j < 1000; j++) {
+//   const monster = new Monster();
+//   const character = new Character('');
+//   character.levelUp();
+//   character.levelUp();
+
+//   let res = true;
+
+//   for (let i = 0; i < 100; i++) {
+//     const previousLife = monster.lifePoints;
+//     if (previousLife <= 0) break;
+//     character.attack(monster);
+//     console.log(character.strength > previousLife);
+//     res = character.strength > previousLife ? monster.lifePoints <= 0 : monster.lifePoints <= previousLife && (monster.lifePoints >= previousLife - (character.strength) || monster.lifePoints == -1);
+//     if (!res) return false;
+//   }
+// }
